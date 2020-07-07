e7a91f39495dcc5ac39b8228cc570a195e0379c7
MADCAT <orioncloud2020@gmail.com>
2020-07-07 11:17:00 +0300

    küçük değişiklikler

--- assignmet(if statments).py
+++ assignmet(if statments).py
@@ -6,9 +6,13 @@ Created on Tue Jul  7 10:33:11 2020
 """
 
 my_name = ("Fatih",)
+
 name = input("What is your name?  : ").strip().title()
 
 if name == my_name :
-    print (f"Hello, {name}!. The password is : W@12")
+
+    print(f"Hello, {name}!. The password is : W@12")
+
 else :
+
     print(f"Hello, {name}! See you later.")
