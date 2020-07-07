978ae21892e0d2a69ec569de80a1c2ff766829ff
MADCAT <orioncloud2020@gmail.com>
2020-07-07 11:05:14 +0300

    if assignment-1 in python

---
+++ assignmet(if statments).py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  7 10:33:11 2020
+
+@author: MADCAT
+"""
+
+my_name = ("Fatih",)
+name = str(input("What is your name?  : ").strip().title())
+
+if name == my_name :
+    print (f"Hello, {name}!. The password is : W@12")
+else :
+    print(f"Hello, {name}! See you later.")
